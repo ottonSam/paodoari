f657e251739a6ccf4062a5c3b861f391a3b7145e
ottonSam <ottonsam@gmail.com>
2021-03-17 03:01:07 -0300

    Get data for user

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '../../services/api';
 
 import Logout from '../../components/Logout';
 
 // import { Container } from './styles';
 
 function Homepage() {
+
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get('/users');
+      setUser(data);
+    })();
+  }, []);  
+
   return(
     <div>
       <h1>Pagina de inicio</h1>
+      <ul>
+          <li>({user.id})</li>
+          <li>({user.usuario})</li>
+          <li>({user.telefone})</li>
+      </ul>
       <Logout />
     </div>
   );
