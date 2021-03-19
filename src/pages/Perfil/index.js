7450951bc39e95a09efd281ea5e2060f87d8d488
ottonSam <ottonsam@gmail.com>
2021-03-19 07:34:40 -0300

    get data for perfil

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '../../services/api';
 
 import Header from '../../components/Header';
 
 // import { Container } from './styles';
 
 function Perfil() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const userData = await api.get('/users');
+      setUser(userData.data);
+    })();
+  }, []);  
   return(
     <div>
       <Header />
       <h1>Perfil</h1>
+      <p>{user.email}</p>
+      <p>{user.cidade}</p>
+      <p>{user.cpf}</p>
+      <p>{user.estado}</p>
+      <p>{user.logadouro}</p>
+      <p>{user.numero}</p>
+      <p>{user.ponto_de_referencia}</p>
+      <p>{user.telefone}</p>
+      <p>{user.data_de_nascimento}</p>
+      <p>{user.usuario}</p>
     </div>
   );
 }
